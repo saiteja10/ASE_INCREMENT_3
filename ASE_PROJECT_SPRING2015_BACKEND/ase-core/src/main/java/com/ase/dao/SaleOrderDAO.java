565b9bc1da24cdcb59caf1324f992147f389dc25
saiteja10 <ss6q9@mail.umkc.edu>
2015-04-13 23:59:05 -0500

    files added

---
+++ ASE_PROJECT_SPRING2015_BACKEND/ase-core/src/main/java/com/ase/dao/SaleOrderDAO.java
@@ -0,0 +1,29 @@
+package com.ase.dao;
+
+import com.ase.domain.OrderStatus;
+import com.ase.domain.SaleOrder;
+import com.ase.query.SaleOrderQuery;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by Gurrala on 4/5/2015.
+ */
+@Component
+public class SaleOrderDAO extends BaseDAO<SaleOrder, SaleOrderQuery> {
+    @Override
+    protected Class<SaleOrder> getEntityClass() {
+        return SaleOrder.class;
+    }
+
+    public List<SaleOrder> getOpenedOrders() {
+        Criteria criteria = createCriteria();
+        criteria.add(Restrictions.eq("orderStatus", OrderStatus.PLACED));
+        criteria.addOrder(Order.asc("id"));
+        return criteria.list();
+    }
+}
